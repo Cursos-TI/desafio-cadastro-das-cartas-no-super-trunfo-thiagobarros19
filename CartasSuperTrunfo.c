a907afb1b484216dfaac0270f2803661dfd31044
Thiago Barros <thiago.rck00@gmail.com>
2025-02-19 15:28:46 -0300

    Desafio aventureiro - Adicionando Menu do Jogo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -75,10 +75,95 @@ int main() {
     printf("PIB da cidade: %.2f\n", pib_2);
     printf("Numero de pontos turisticos da cidade: %d\n", touristAttractionAmount_2);
 
-    if (pibPerCapita_1 > pibPerCapita_2) {
-        printf("Cidade %s Venceu!\n", name_1);
-    } else {
-        printf("Cidade %s Venceu!\n", name_2);
+    printf("-------------------------------------\n");
+
+    printf("Escolha qual será o parâmetro de comparação entre as cartas: \n");
+    printf("1 - População\n");
+    printf("2 - Area\n");
+    printf("3 - PIB\n");
+    printf("4 - Pontos Turísticos\n");
+    printf("5 - Densidade Populacional\n");
+    printf("6 - PIB per Capita\n");
+
+    int choice;
+    scanf("%d", &choice);
+
+    switch (choice) {
+        case 1:
+            printf("Nome das cidades: %s, %s\n", name_1, name_2);
+            printf("Parâmetro selecionado: População\n");
+            printf("Valores das cartas: %d, %d\n", population_1, population_2);
+            if (population_1 > population_2) {
+                printf("Cidade %s Venceu!\n", name_1);
+            } else if (population_2 > population_1) {
+                printf("Cidade %s Venceu!\n", name_2);
+            } else {
+                printf("Empate!\n");
+            }
+            break;
+        case 2:
+            printf("Nome das cidades: %s, %s\n", name_1, name_2);
+            printf("Parâmetro selecionado: Area\n");
+            printf("Valores das cartas: %.2f, %.2f\n", area_1, area_2);
+            if (area_1 > area_2) {
+                printf("Cidade %s Venceu!\n", name_1);
+            } else if (area_2 > area_1) {
+                printf("Cidade %s Venceu!\n", name_2);
+            } else {
+                printf("Empate!\n");
+            }
+            break;
+        case 3:
+            printf("Nome das cidades: %s, %s\n", name_1, name_2);
+            printf("Parâmetro selecionado: PIB\n");
+            printf("Valores das cartas: %.2f, %.2f\n", pib_1, pib_2);
+            if (pib_1 > pib_2) {
+                printf("Cidade %s Venceu!\n", name_1);
+            } else if (pib_2 > pib_1) {
+                printf("Cidade %s Venceu!\n", name_2);
+            } else {
+                printf("EMPATE!\n");
+            }
+            break;
+        case 4:
+            printf("Nome das cidades: %s, %s\n", name_1, name_2);
+            printf("Parâmetro selecionado: Número de pontos turísticos\n");
+            printf("Valores das cartas: %d, %d\n", touristAttractionAmount_1, touristAttractionAmount_2);
+            if (touristAttractionAmount_1 > touristAttractionAmount_2) {
+                printf("Cidade %s Venceu!\n", name_1);
+            } else if (touristAttractionAmount_2 > touristAttractionAmount_1) {
+                printf("Cidade %s Venceu!\n", name_2);
+            } else {
+                printf("EMPATE!\n");
+            }
+            break;
+        case 5:
+            printf("Nome das cidades: %s, %s\n", name_1, name_2);
+            printf("Parâmetro selecionado: Densidade Populacional\n");
+            printf("Valores das cartas: %.2f, %.2f\n", populationDensity_1, populationDensity_2);
+            if (populationDensity_1 < populationDensity_2) {
+                printf("Cidade %s Venceu!\n", name_1);
+            } else if (populationDensity_2 > populationDensity_1) {
+                printf("Cidade %s Venceu!\n", name_2);
+            } else {
+                printf("EMPATE!\n");
+            }
+            break;
+        case 6:
+            printf("Nome das cidades: %s, %s\n", name_1, name_2);
+            printf("Parâmetro selecionado: PIB per capita\n");
+            printf("Valores das cartas: %.2f, %.2f\n", pibPerCapita_1, pibPerCapita_2);
+            if (pibPerCapita_1 > pibPerCapita_2) {
+                printf("Cidade %s Venceu!\n", name_1);
+            } else if (pibPerCapita_2 > pibPerCapita_1) {
+                printf("Cidade %s Venceu!\n", name_2);
+            } else {
+                printf("EMPATE!\n");
+            }
+            break;
+        default:
+            printf("Opção inválida!\n");
+            break;
     }
 
     return 0;
